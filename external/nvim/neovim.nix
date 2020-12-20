3690d8a637dc891704d5681d535dc660f83fb66b
Akshay Nair <phenax5@gmail.com>
2020-12-20 16:50:23 +0530

    Adds neovim config as submodule

---
+++ external/nvim/neovim.nix
@@ -0,0 +1,19 @@
+self: super: {
+  tree-sitter-updated = super.tree-sitter.overrideAttrs(oldAttrs: {
+    postInstall = ''
+      PREFIX=$out make install;
+    '';
+  });
+  neovim-unwrapped = super.neovim-unwrapped.overrideAttrs (oldAttrs: rec {
+    name = "neovim-nightly";
+    version = "0.5-nightly";
+    src = self.fetchFromGitHub {
+      owner = "neovim";
+      repo = "neovim";
+      rev = "nightly";
+      sha256 = "0vpjdd32lgzyh85gyazqpms8vmaad6px3zx2svdxhvcdxgschqz9";
+    };
+
+    nativeBuildInputs = with self.pkgs; [ unzip cmake pkgconfig gettext tree-sitter-updated ];
+  });
+}
