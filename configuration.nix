3f07b159c4f5d03066699001cbc13d8725e75d71
Akshay Nair <phenax5@gmail.com>
2020-12-21 21:19:43 +0530

    Removes picom

--- configuration.nix
+++ configuration.nix
@@ -118,22 +118,22 @@ ${c1}         ▝▀▀▀    ▀▀▀▀▘       ${c2}▀▀▀▘${res}
     cozette
     noto-fonts-emoji
   ];
-  services.picom = {
-    enable = true;
-    inactiveOpacity = 0.8;
-    backend = "glx";
-    settings = {
-      "inactive-dim" = 0.3;
-      "focus-exclude" = [ "class_g = 'dwm'" "class_g = 'dmenu'"];
-    };
-    opacityRules = [
-      "98:class_g = 'St' && focused"
-      "85:class_g = 'St' && !focused"
-      "90:class_g = 'qutebrowser' && !focused"
-      "100:class_g = 'qutebrowser' && focused"
-    ];
-    menuOpacity = 0.9;
-  };
+  #services.picom = {
+    #enable = true;
+    #inactiveOpacity = 0.8;
+    #backend = "glx";
+    #settings = {
+      #"inactive-dim" = 0.3;
+      #"focus-exclude" = [ "class_g = 'dwm'" "class_g = 'dmenu'"];
+    #};
+    #opacityRules = [
+      #"98:class_g = 'St' && focused"
+      #"85:class_g = 'St' && !focused"
+      #"90:class_g = 'qutebrowser' && !focused"
+      #"100:class_g = 'qutebrowser' && focused"
+    #];
+    #menuOpacity = 0.9;
+  #};
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
--- packages.nix
+++ packages.nix
@@ -62,7 +62,7 @@ let
     pciutils
     udiskie
     feh
-    picom
+    # picom
     ffmpeg-full
     # nm-applet
 
