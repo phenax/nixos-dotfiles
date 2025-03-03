90862ce996ee704042333532f601eef08588722f
Akshay Nair <phenax5@gmail.com>
2025-03-03 12:43:26 +0530

    St reworked to flake

--- packages/st/pkg.nix
+++ packages/st/pkg.nix
@@ -1,17 +1,28 @@
-{ stdenv, pkgs, libX11, libXft, fontconfig, pkg-config, ncurses, imlib2 }:
+# { stdenv, libX11, libXft, fontconfig, freetype, pkg-config, ncurses, imlib2 }:
+#
+# stdenv.mkDerivation {
+#   pname = "local-st";
+#   version = "0.9.4";
+#
+#   src = ./source;
+#
+#   strictDeps = true;
+#   nativeBuildInputs = [ pkg-config fontconfig freetype ncurses ];
+#   buildInputs = [ libX11 libXft fontconfig imlib2 ];
+#
+#   outputs = [ "out" ];
+#
+#   unpackPhase = ''cp -r $src/* .'';
+#
+#   makeFlags = [
+#     "PKG_CONFIG=${stdenv.cc.targetPrefix}pkg-config"
+#   ];
+#   buildPhase = ''make'';
+#
+#   installFlags = [ "PREFIX=$(out)" "DESTDIR=''" ];
+#   installPhase = ''TERMINFO=$out/share/terminfo make install'';
+# }
 
-stdenv.mkDerivation rec {
-  name = "local-st-${version}";
-  version = "0.9.4";
+{...}:
+(builtins.getFlake "path:/home/imsohexy/nixos/packages/st/source").packages.x86_64-linux.default
 
-  src = ./source;
-
-  nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ libX11 libXft fontconfig ncurses imlib2 ];
-
-  unpackPhase = ''cp -r $src/* .'';
-
-  buildPhase = ''make'';
-
-  installPhase = ''TERMINFO=$out/share/terminfo make PREFIX=$out DESTDIR="" install'';
-}
